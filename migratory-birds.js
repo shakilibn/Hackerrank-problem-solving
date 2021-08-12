88091fd8ffec8a1132842465a7999338df8ac1c4
shakilibn <mdsh540@gmail.com>
2021-08-13 01:48:23 +0600

    migratory birds is done

--- migratory-birds.js
+++ migratory-birds.js
@@ -1,9 +1,28 @@
 const migratoryBirds = (arr) => {
-  let arrObj = {};
-  for (let num of arr) {
-    arrObj[num] = arrObj[num] + 1 || 1;
-  }
-  console.log(arrObj);
+  const allBirds = {};
+  const mostSighted = [];
+  let mostCommon = [0, 0];
+
+  arr.forEach((bird) => {
+    if (!allBirds[bird]) {
+      allBirds[bird] = 1;
+    } else {
+      allBirds[bird] = allBirds[bird] + 1;
+    }
+    if (allBirds[bird] > mostCommon[1]) {
+      mostCommon[0] = bird;
+      mostCommon[1] = allBirds[bird];
+    }
+  });
+
+  Object.keys(allBirds).forEach((key) => {
+    if (allBirds[key] === mostCommon[1]) {
+      mostSighted.push(key);
+    }
+  });
+
+  mostSighted.sort();
+  return mostSighted[0];
 };
 
 const array = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4];
