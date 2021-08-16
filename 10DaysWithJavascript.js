9bb5fb82beef4eddf73c6a99bb4526866ad27824
shakilibn <mdsh540@gmail.com>
2021-08-16 14:44:39 +0600

    template literals is done

--- 10DaysWithJavascript.js
+++ 10DaysWithJavascript.js
@@ -112,18 +112,33 @@
 // let Spot = new Animal("spot");
 // Spot.speak();
 
-class Polygon {
-  constructor(a) {
-    this.a = a;
-  }
-  perimeter() {
-    let pm = 0;
-    this.a.forEach((element) => {
-      pm += element;
-    });
-    return pm;
-  }
-}
+// class Polygon {
+//   constructor(a) {
+//     this.a = a;
+//   }
+//   perimeter() {
+//     let pm = 0;
+//     this.a.forEach((element) => {
+//       pm += element;
+//     });
+//     return pm;
+//   }
+// }
 
-let triangle = new Polygon([3, 4, 5]);
-console.log(triangle.perimeter());
+// let triangle = new Polygon([3, 4, 5]);
+// console.log(triangle.perimeter());
+
+// function sides(literals, ...expressions) {
+//   var area = expressions[0];
+//   var perimeter = expressions[1];
+
+//   var s1 = (perimeter + Math.sqrt(perimeter * perimeter - 16 * area)) / 4;
+//   //console.log("s1: " + s1);
+//   var s2 = (perimeter - Math.sqrt(perimeter * perimeter - 16 * area)) / 4;
+//   //console.log("s2: " + s2);
+//   var array = [s1, s2];
+//   array = array.sort(function (a, b) {
+//     return a - b;
+//   });
+//   return array;
+// }
