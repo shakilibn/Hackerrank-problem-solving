52f57ad4945918dd4b1fda80c3a2b64f6e0b6260
shakilibn <mdsh540@gmail.com>
2022-01-03 22:52:15 +0600

    number line jumps problem solved

---
+++ numberLineJumps.js
@@ -0,0 +1,13 @@
+function kangaroo(x1, v1, x2, v2) {
+  let n = 0;
+
+  while (n < 10000) {
+    if (x1 + n * v1 === x2 + n * v2) {
+      return "YES";
+    }
+    n++;
+  }
+  return "NO";
+}
+
+console.log(kangaroo(0, 3, 4, 2));
